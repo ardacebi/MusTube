22bdd39659e600e24bb81ab66089ba7d86e0fba4
Arda Çebi <ardacebi1@gmail.com>
2018-04-23 20:17:07 +0300

    Upload

--- mobile/src/main/java/com/ardacebi/teltube/Main2Activity.java
+++ mobile/src/main/java/com/ardacebi/teltube/Main2Activity.java
@@ -61,6 +61,8 @@ public class Main2Activity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
 
+
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
